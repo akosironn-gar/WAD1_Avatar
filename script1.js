5412a1e9216938ee21a50ab5df9aaac998aa5504
Ronn Gardoce <ronnzigmund.gardoce@student.laverdad.edu.ph>
2026-08-25 16:46:18 +0800

    Profile Card

--- script1.js
+++ script1.js
@@ -1,20 +1,20 @@
 const profileContainer = document.getElementById("profile-container");
 const profileCard = document.createElement("div");
-const avatar = document.createElement("img");
+const img = document.createElement("img");
 const name = document.createElement("h2");
 const bio = document.createElement("p");
 
-avatar.src = "image.png";
-avatar.alt = "Ronn Gardoce";
+img.src = "image.png";
+img.alt = "Ronn Gardoce";
 name.textContent = "Ronn Gardoce";
 bio.textContent = "BSIS Student | Aspiring AI Engineer";
 
 profileCard.className = "profile-card";
-avatar.className = "avatar";
+img.className = "avatar";
 name.className = "student-name";
 bio.className = "student-bio";
 
-profileCard.appendChild(avatar);
+profileCard.appendChild(img);
 profileCard.appendChild(name);
 profileCard.appendChild(bio);
 
