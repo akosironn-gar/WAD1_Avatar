c886a38461735b89a36515f658982b36660147d0
Ronn Gardoce <ronnzigmund.gardoce@student.laverdad.edu.ph>
2026-09-09 13:43:25 +0800

    Interactive Profile

--- script1.js
+++ script1.js
@@ -1,4 +1,7 @@
+// Create the profile card
+
 const profileContainer = document.getElementById("profile-container");
+
 const profileCard = document.createElement("div");
 const img = document.createElement("img");
 const name = document.createElement("h2");
@@ -6,6 +9,7 @@ const bio = document.createElement("p");
 
 img.src = "image.png";
 img.alt = "Ronn Gardoce";
+
 name.textContent = "Ronn Gardoce";
 bio.textContent = "BSIS Student | Aspiring AI Engineer";
 
@@ -18,4 +22,52 @@ profileCard.appendChild(img);
 profileCard.appendChild(name);
 profileCard.appendChild(bio);
 
-profileContainer.appendChild(profileCard); 
+profileContainer.appendChild(profileCard);
+
+
+//Live Name Preview
+
+const nameInput = document.getElementById("nameInput");
+const welcomeMessage = document.getElementById("welcomeMessage");
+
+nameInput.addEventListener("input", function() {
+    welcomeMessage.textContent = "Hello, " + nameInput.value;
+});
+
+
+//Handle Form Submission
+
+const studentForm = document.getElementById("studentForm");
+const courseInput = document.getElementById("courseInput");
+
+studentForm.addEventListener("submit", function(event) {
+
+    event.preventDefault();
+
+    welcomeMessage.textContent =
+        "Welcome " + nameInput.value + "! You are registered in " + courseInput.value + ".";
+});
+
+
+//Toggle the Theme
+
+const themeButton = document.getElementById("themeButton");
+
+themeButton.addEventListener("click", function() {
+
+    document.body.classList.toggle("dark");
+
+});
+
+
+//Keyboard Challenge
+
+document.addEventListener("keydown", function(event) {
+
+    if (event.key === "Escape") {
+
+        welcomeMessage.textContent = "";
+
+    }
+
+});
